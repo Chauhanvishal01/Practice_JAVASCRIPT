bd56c9d9ed202a3cd335682081744c91896edbc3
Vishal Chauhan <vishal2825m@gmail.com>
2024-07-26 06:05:11 +0530

    Day 2

---
+++ 30_Days_JS/Day_2/arithmetic.js
@@ -0,0 +1,32 @@
+const firstNumber = 20;
+const secondNumber = 5;
+
+console.log(
+  `Sum of ${firstNumber} and ${secondNumber} is`,
+  firstNumber + secondNumber
+);
+console.log(
+  `Subtraction of ${firstNumber} and ${secondNumber} is`,
+  firstNumber - secondNumber
+);
+console.log(
+  `Multiplication of ${firstNumber} and ${secondNumber} is`,
+  firstNumber * secondNumber
+);
+console.log(
+  `Division of ${firstNumber} and ${secondNumber} is`,
+  firstNumber / secondNumber
+);
+console.log(
+  `Remainder of ${firstNumber} and ${secondNumber} is`,
+  firstNumber % secondNumber
+);
+
+
+let thirdNumber=20
+thirdNumber += 10;
+
+console.log(thirdNumber);
+thirdNumber -= 20;
+console.log(thirdNumber);
+
