e5a1dd2351766d4cb98f2230ac96d8eb8d573c02
Vishal Chauhan <vishal2825m@gmail.com>
2024-07-26 06:05:42 +0530

    Day 2

---
+++ 30_Days_JS/Day_2/comparison.js
@@ -0,0 +1,16 @@
+let firstNumber=99
+let secondNumber=199
+
+console.log( firstNumber < secondNumber);
+console.log(firstNumber > secondNumber);
+console.log(firstNumber <= secondNumber);
+console.log(firstNumber >= secondNumber);
+console.log(firstNumber == secondNumber);
+console.log(firstNumber === secondNumber);
+
+let thirdNumber=123
+let str="123"
+
+console.log("hello",thirdNumber == str);
+console.log("hello",thirdNumber === str);
+
