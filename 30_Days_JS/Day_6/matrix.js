2bcd464a2bf761b6e134b41217e2f352c6d3ce63
Vishal Chauhan <vishal2825m@gmail.com>
2024-08-11 08:46:30 +0530

    Day 6

---
+++ 30_Days_JS/Day_6/matrix.js
@@ -0,0 +1,13 @@
+let matrix = [
+  ["Vishal", 18, "Male", "Btech"],
+  ["Nitin", 28, "Male", "Bcom"],
+  ["Sara", 18, "Female", "BSC"],
+];
+
+for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 4; j++) {
+    console.log(matrix[i][j]);
+  }
+}
+
+console.log(matrix[2][0]);
