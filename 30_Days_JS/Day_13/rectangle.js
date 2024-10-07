4621650ba6aa0f47a040ce8efb304cb5b15e6df6
Vishal Chauhan <vishal2825m@gmail.com>
2024-10-07 14:57:29 +0530

    Day 13

---
+++ 30_Days_JS/Day_13/rectangle.js
@@ -0,0 +1,5 @@
+var prompt = require("prompt-sync")();
+const length = prompt("Enter the length :");
+const width = prompt("Enter the width:");
+
+console.log("Area of rectangle is:", length * width);
